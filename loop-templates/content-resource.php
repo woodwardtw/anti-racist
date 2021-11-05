fffb23375e3e849b171574f11060efcd46903dfa
woodwardtw <twwoodward@me.com>
2021-11-05 08:37:36 -0400

    single cpt displays
    add estimated consumption time for resource

---
+++ loop-templates/content-resource.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Single resource partial template
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+?>
+
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+	<header class="entry-header">
+		<a href="<?php the_field('link');?>">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		</a>
+
+		<div class="entry-meta">
+
+			<?php //understrap_posted_on(); ?>
+
+		</div><!-- .entry-meta -->
+
+	</header><!-- .entry-header -->
+
+	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+
+	<div class="entry-content">
+
+		<?php
+		the_field('summary');
+		the_content();
+		understrap_link_pages();
+		?>
+
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+
+		<?php understrap_entry_footer(); ?>
+
+	</footer><!-- .entry-footer -->
+
+</article><!-- #post-## -->
