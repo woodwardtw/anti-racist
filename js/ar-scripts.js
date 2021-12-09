b208570fe1d15bb014af94f371d3e4a2c4114839
woodwardtw <twwoodward@me.com>
2021-12-09 11:11:34 -0500

    facet reload trigger triangle js

--- js/ar-scripts.js
+++ js/ar-scripts.js
@@ -1,20 +1,32 @@
 
 
 window.addEventListener("load", function(){
-    console.log('tri tri tri loaded')
-if(document.querySelectorAll('polygon')){
-  console.log('tri tri tri')
-  const paths = document.querySelectorAll('polygon');
+  colorTheTriangles();
+});
 
-  paths.forEach((path) => {
-    const color = randomColor();
-    path.classList.add(color)
-    });
 
-  function randomColor(){
-    const classes = ['red','green','blue','yellow','black']
-    const number = Math.floor(Math.random()*classes.length);
-    return classes[number]
+//deal with facet reload trigger
+(function($) {
+  document.addEventListener('facetwp-loaded', function() {
+    colorTheTriangles();
+   });
+})(jQuery);
+
+
+
+function colorTheTriangles(){
+  if(document.querySelectorAll('polygon')){
+    const paths = document.querySelectorAll('polygon');
+  
+    paths.forEach((path) => {
+      const color = randomColor();
+      path.classList.add(color)
+      });
+  
+    function randomColor(){
+      const classes = ['red','green','blue','yellow','black']
+      const number = Math.floor(Math.random()*classes.length);
+      return classes[number]
+    }
   }
 }
-});
